fa3921c45289c67836a30ee02e3fcd0d31733bd7
br79641 <rollerbrian@johndeere.com>
2023-03-22 16:45:40 -0500

    remove unneeded files

--- deca/fast_file_2.py
+++ deca/fast_file_2.py
@@ -1,6 +1,4 @@
-from typing import List
 import numpy as np
-from numba import njit, jit
 
 
 class FFError(Exception):
@@ -12,16 +10,11 @@ params = {
     'nogil': True,
 }
 
-@njit
 def raise_error():
     raise FFError('ff_read: not enough data')
 
 
-@njit(**params)
 def ff_read(bufn, pos, n):
-    # buffer = ff
-    # n_buffer = len(buffer)
-
     if bufn[1] >= (pos + n):
         ret = bufn[0][pos:(pos + n)]
         return ret, pos + n
@@ -33,7 +26,6 @@ def make_read_one(data_type):
     dt = np.dtype(data_type)
     ele_size = dt.itemsize
 
-    @njit(**params)
     def f(bufn, pos):
         new_pos = pos + ele_size
         if new_pos > bufn[1]:
@@ -48,7 +40,6 @@ def make_read_many(data_type):
     dt = np.dtype(data_type)
     ele_size = dt.itemsize
 
-    @njit(**params)
     def f(bufn, pos, count):
         new_pos = pos + ele_size * count
         if new_pos > bufn[1]:
@@ -82,7 +73,6 @@ ff_read_f32s = make_read_many(np.float32)
 ff_read_f64s = make_read_many(np.float64)
 
 
-@njit(**params)
 def ff_read_strz(bufn, pos):
     pos0 = pos
     while bufn[0][pos] != 0 and pos < bufn[1]:
--- deca/hashes.py
+++ deca/hashes.py
@@ -1,8 +1,3 @@
-import sys
-import mmh3
-import numpy as np
-from numba import njit
-
 # Need to constrain U32 to only 32 bits using the & 0xffffffff
 # since Python has no native notion of integers limited to 32 bit
 # http://docs.python.org/library/stdtypes.html#numeric-types-int-float-long-complex
@@ -32,12 +27,10 @@ class CostModel(object):
 cost_model_params = 0
 
 
-@njit(inline=CostModel(cost_model_params))
 def rot(x, k):
     return (x << k) | (x >> (32 - k))
 
 
-@njit(inline=CostModel(cost_model_params))
 def mix(a, b, c):
     a &= 0xffffffff; b &= 0xffffffff; c &= 0xffffffff
     a -= c; a &= 0xffffffff; a ^= rot(c,4);  a &= 0xffffffff; c += b; c &= 0xffffffff
@@ -49,7 +42,6 @@ def mix(a, b, c):
     return a, b, c
 
 
-@njit(inline=CostModel(cost_model_params))
 def final(a, b, c):
     a &= 0xffffffff; b &= 0xffffffff; c &= 0xffffffff
     c ^= b; c &= 0xffffffff; c -= rot(b,14); c &= 0xffffffff
@@ -62,7 +54,6 @@ def final(a, b, c):
     return a, b, c
 
 
-@njit(inline=CostModel(cost_model_params))
 def hashlittle2(data, initval=0, initval2=0):
     length = lenpos = len(data)
 
@@ -100,7 +91,6 @@ def hashlittle2(data, initval=0, initval2=0):
     return c, b
 
 
-@njit(inline=CostModel(cost_model_params))
 def hash32_func_bytes(data, init_val=0):
     c, b = hashlittle2(data, init_val, 0)
     return c
@@ -110,190 +100,3 @@ def hash32_func(data, init_val=0):
     if isinstance(data, str):
         data = data.encode('ascii')
     return hash32_func_bytes(data, init_val)
-
-
-def hash48_func(data):
-    if isinstance(data, str):
-        data = data.encode('ascii')
-
-    v = mmh3.hash128(key=data, x64arch=True)
-    return (v >> 16) & 0x0000FFFFFFFFFFFF
-
-
-def hash64_func(data):
-    if isinstance(data, str):
-        data = data.encode('ascii')
-
-    v = mmh3.hash128(key=data, x64arch=True)
-    return int(np.int64(np.uint64(v & 0xFFFFFFFFFFFFFFFF)))
-
-
-def hash_all_func(data):
-    if isinstance(data, str):
-        data = data.encode('ascii')
-
-    c, b = hashlittle2(data, 0, 0)
-
-    v = mmh3.hash128(key=data, x64arch=True)
-
-    return c, (v >> 16) & 0x0000FFFFFFFFFFFF, int(np.int64(np.uint64(v & 0xFFFFFFFFFFFFFFFF)))
-
-
-def main():
-    data = sys.argv[1]
-
-    hv = hash32_func(data)
-    print('hash4 "{}" = {:12} , 0x{:08x}'.format(data, hv, hv))
-
-    hv = hash48_func(data)
-    print('hash6 "{}" = {:24} , 0x{:012x}'.format(data, hv, hv))
-
-    hv = hash64_func(data)
-    print('hash8 "{}" = {:24}, {:24}, 0x{:016x}'.format(data,  hv, np.uint64(hv), np.uint64(hv)))
-
-
-if __name__ == "__main__":
-    main()
-
-
-'''
-JENKINS HASH code from 
-https://github.com/gibbed/Gibbed.JustCause3/blob/master/projects/Gibbed.JustCause3.FileFormats/StringHelpers.cs
-'''
-
-'''
-        private static uint HashJenkins(byte[] data, int index, int length, uint seed)
-        {
-            // ReSharper disable JoinDeclarationAndInitializer
-            uint a, b, c;
-            // ReSharper restore JoinDeclarationAndInitializer
-
-            a = b = c = 0xDEADBEEF + (uint)length + seed;
-
-            int i = index;
-            while (i + 12 < length)
-            {
-                // ReSharper disable RedundantCast
-                a += (uint)data[i++] |
-                     ((uint)data[i++] << 8) |
-                     ((uint)data[i++] << 16) |
-                     ((uint)data[i++] << 24);
-                b += (uint)data[i++] |
-                     ((uint)data[i++] << 8) |
-                     ((uint)data[i++] << 16) |
-                     ((uint)data[i++] << 24);
-                c += (uint)data[i++] |
-                     ((uint)data[i++] << 8) |
-                     ((uint)data[i++] << 16) |
-                     ((uint)data[i++] << 24);
-                // ReSharper restore RedundantCast
-                a -= c;
-                a ^= (c << 4) | (c >> (32 - 4));
-                c += b;
-                b -= a;
-                b ^= (a << 6) | (a >> (32 - 6));
-                a += c;
-                c -= b;
-                c ^= (b << 8) | (b >> (32 - 8));
-                b += a;
-                a -= c;
-                a ^= (c << 16) | (c >> (32 - 16));
-                c += b;
-                b -= a;
-                b ^= (a << 19) | (a >> (32 - 19));
-                a += c;
-                c -= b;
-                c ^= (b << 4) | (b >> (32 - 4));
-                b += a;
-            }
-
-            if (i < length)
-            {
-                a += data[i++];
-            }
-
-            if (i < length)
-            {
-                a += (uint)data[i++] << 8;
-            }
-
-            if (i < length)
-            {
-                a += (uint)data[i++] << 16;
-            }
-
-            if (i < length)
-            {
-                a += (uint)data[i++] << 24;
-            }
-
-            if (i < length)
-            {
-                // ReSharper disable RedundantCast
-                b += (uint)data[i++];
-                // ReSharper restore RedundantCast
-            }
-
-            if (i < length)
-            {
-                b += (uint)data[i++] << 8;
-            }
-
-            if (i < length)
-            {
-                b += (uint)data[i++] << 16;
-            }
-
-            if (i < length)
-            {
-                b += (uint)data[i++] << 24;
-            }
-
-            if (i < length)
-            {
-                // ReSharper disable RedundantCast
-                c += (uint)data[i++];
-                // ReSharper restore RedundantCast
-            }
-
-            if (i < length)
-            {
-                c += (uint)data[i++] << 8;
-            }
-
-            if (i < length)
-            {
-                c += (uint)data[i++] << 16;
-            }
-
-            if (i < length)
-            {
-                c += (uint)data[i /*++*/] << 24;
-            }
-
-            c ^= b;
-            c -= (b << 14) | (b >> (32 - 14));
-            a ^= c;
-            a -= (c << 11) | (c >> (32 - 11));
-            b ^= a;
-            b -= (a << 25) | (a >> (32 - 25));
-            c ^= b;
-            c -= (b << 16) | (b >> (32 - 16));
-            a ^= c;
-            a -= (c << 4) | (c >> (32 - 4));
-            b ^= a;
-            b -= (a << 14) | (a >> (32 - 14));
-            c ^= b;
-            c -= (b << 24) | (b >> (32 - 24));
-
-            return c;
-        }
-        #endregion
-
-        public static uint HashJenkins(this string input)
-        {
-            byte[] data = Encoding.ASCII.GetBytes(input);
-            return HashJenkins(data, 0, data.Length, 0);
-        }
-
-'''
